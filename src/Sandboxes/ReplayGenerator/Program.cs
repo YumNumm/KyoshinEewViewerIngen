8d38f311149a6beeddbceffd9472d86fef6bd872
Ryotaro <onory0917@gmail.com>
2026-04-21 03:24:33 +0900

    fix: ReplayGeneratorでConfigureKestrel拡張が解決できるようusingを追加

--- src/Sandboxes/ReplayGenerator/Program.cs
+++ src/Sandboxes/ReplayGenerator/Program.cs
@@ -1,6 +1,7 @@
 using System.Globalization;
 using System.Net;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
